a476bc4a72b52fdfb459eff70047958ee69d4184
WildBamaBoy <wildbamaboy@gmail.com>
2015-05-21 14:58:20 -0500

    Fix console spam due to sync issues. Fixes #12

--- src/main/java/radixcore/packets/PacketDataSyncReq.java
+++ src/main/java/radixcore/packets/PacketDataSyncReq.java
@@ -11,7 +11,7 @@ import cpw.mods.fml.common.network.simpleimpl.MessageContext;
 public class PacketDataSyncReq extends AbstractPacket implements IMessage, IMessageHandler<PacketDataSyncReq, IMessage>
 {
 	private int entityId;
-	
+
 	public PacketDataSyncReq()
 	{
 	}
@@ -39,16 +39,19 @@ public class PacketDataSyncReq extends AbstractPacket implements IMessage, IMess
 		try
 		{
 			IWatchable watchable = (IWatchable) context.getServerHandler().playerEntity.worldObj.getEntityByID(packet.entityId);
-			DataWatcherEx dataWatcherEx = watchable.getDataWatcherEx();
-			
-			return new PacketDataSync(packet.entityId, dataWatcherEx);
+
+			if (watchable != null) //Can be null, assuming it's a client-side sync issue. Doesn't seem to affect anything.
+			{
+				DataWatcherEx dataWatcherEx = watchable.getDataWatcherEx();
+				return new PacketDataSync(packet.entityId, dataWatcherEx);
+			}
 		}
-		
+
 		catch (Throwable e)
 		{
 			RadixExcept.logErrorCatch(e, "Error sending sync data to client.");
 		}
-		
+
 		return null;
 	}
 }
