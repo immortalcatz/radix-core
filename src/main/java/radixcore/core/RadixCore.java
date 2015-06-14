b9c618b706f0eca7281036acc0a0a70ff64c9afd
WildBamaBoy <wildbamaboy@gmail.com>
2015-06-13 20:47:16 -0500

    Update Forge and specify 1.8 variant.

--- src/main/java/radixcore/core/RadixCore.java
+++ src/main/java/radixcore/core/RadixCore.java
@@ -22,7 +22,7 @@ public class RadixCore
 {
 	protected static final String ID = "RadixCore";
 	protected static final String NAME = "RadixCore";
-	protected static final String VERSION = "2.0.2";
+	protected static final String VERSION = "2.0.2.1-1.8";
 
 	@Instance(ID)
 	private static RadixCore instance;
