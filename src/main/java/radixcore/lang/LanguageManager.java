b75a4679684ddf1b9a500ae90f851bc723c8a26a
WildBamaBoy <wildbamaboy@gmail.com>
2015-10-11 21:01:47 -0500

    Allow language manager to load additional languages.

--- src/main/java/radixcore/lang/LanguageManager.java
+++ src/main/java/radixcore/lang/LanguageManager.java
@@ -1,20 +1,19 @@
 package radixcore.lang;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.Minecraft;
 import net.minecraft.util.StringTranslate;
 import radixcore.core.RadixCore;
 import radixcore.util.RadixExcept;
 import radixcore.util.RadixMath;
-import cpw.mods.fml.common.FMLCommonHandler;
 
 public class LanguageManager 
 {
@@ -26,51 +25,7 @@ public class LanguageManager
 	{
 		this.modId = providedModId.toLowerCase();
 		this.translationsMap = new HashMap<String, String>();
-		
-		Properties properties = new Properties();
-		
-		if (FMLCommonHandler.instance().getEffectiveSide().isClient())
-		{
-			String languageId = getLanguageIDFromOptions();
-			
-			try
-			{
-				properties.load(StringTranslate.class.getResourceAsStream("/assets/" + modId + "/lang/" + languageId + ".lang"));
-			}
-
-			catch (Exception e)
-			{
-				RadixCore.getLogger().error("Error loading language " + languageId + " for " + modId + ". Attempting to default to English.");
-				
-				try
-				{
-					properties.load(StringTranslate.class.getResourceAsStream("/assets/" + modId + "/lang/" + "en_US.lang"));
-				}
-				
-				catch (Exception e2)
-				{
-					RadixExcept.logFatalCatch(e2, "Error loading language " + languageId + " for mod " + modId + ".");
-				}
-			}
-		}
-		
-		else
-		{
-			try
-			{
-				properties.load(StringTranslate.class.getResourceAsStream("/assets/" + modId + "/lang/" + "en_US.lang"));
-			}
-			
-			catch (Exception e)
-			{
-				RadixExcept.logFatalCatch(e, "Error loading language server-side. Loading cannot continue.");
-			}
-		}
-		
-		for (final Map.Entry<Object, Object> entrySet : properties.entrySet())
-		{
-			translationsMap.put(entrySet.getKey().toString(), entrySet.getValue().toString());
-		}
+		loadLanguage(getGameLanguageID());
 	}
 
 	public LanguageManager(String modId, AbstractLanguageParser parser)
@@ -78,51 +33,20 @@ public class LanguageManager
 		this(modId);
 		this.parser = parser;
 	}
-	
-	public String getLanguageIDFromOptions()
+
+	@SideOnly(Side.CLIENT)
+	public String getGameLanguageID()
 	{
-		BufferedReader reader = null;
-		String languageID = "";
+		String languageID = "en_US";
 
 		try
 		{
-			reader = new BufferedReader(new FileReader(RadixCore.getRunningDirectory() + "/options.txt"));
-
-			String line = null;
-
-			while (line != null)
-			{
-				line = reader.readLine();
-
-				if (line.contains("lang:"))
-				{
-					break;
-				}
-			}
-
-			if (!line.isEmpty())
-			{
-				reader.close();
-				languageID = line.substring(5);
-			}
+			languageID = Minecraft.getMinecraft().getLanguageManager().getCurrentLanguage().getLanguageCode();
 		}
 
-		catch (final FileNotFoundException e)
+		catch (final Exception e)
 		{
-			RadixCore.getLogger().error("Could not find options.txt file. Defaulting to English.");
-			languageID = "en_US";
-		}
-
-		catch (final IOException e)
-		{
-			RadixCore.getLogger().error("Error reading from Minecraft options.txt file. Defaulting to English.", e);
-			languageID = "en_US";
-		}
-
-		catch (final NullPointerException e)
-		{
-			RadixCore.getLogger().error("NullPointerException while trying to read options.txt. Defaulting to English.");
-			languageID = "en_US";
+			RadixCore.getLogger().error("Unable to get current language code. Defaulting to English.");
 		}
 
 		return languageID;
@@ -132,7 +56,7 @@ public class LanguageManager
 	{
 		return getString(id, (Object) null);
 	}
-	
+
 	public String getString(String id, Object... arguments)
 	{
 		//Check if the exact provided key exists in the translations map.
@@ -143,18 +67,18 @@ public class LanguageManager
 			{
 				return parser.parsePhrase(translationsMap.get(id), arguments);
 			}
-			
+
 			else
 			{
 				return translationsMap.get(id);
 			}
 		}
-		
+
 		else
 		{
 			//Build a list of keys that at least contain part of the provided key name.
 			List<String> containingKeys = new ArrayList<String>();
-			
+
 			for (String key : translationsMap.keySet())
 			{
 				if (key.contains(id))
@@ -162,23 +86,23 @@ public class LanguageManager
 					containingKeys.add(key);
 				}
 			}
-			
+
 			//Return a random potentially valid key if some were found.
 			if (containingKeys.size() > 0)
 			{
 				String key = containingKeys.get(RadixMath.getNumberInRange(0, containingKeys.size() - 1));
-				
+
 				if (parser != null)
 				{
 					return parser.parsePhrase(translationsMap.get(key), arguments);
 				}
-				
+
 				else
 				{
 					return translationsMap.get(key);
 				}
 			}
-			
+
 			else
 			{
 				RadixCore.getLogger().error("[" + modId + "] No mapping found for requested phrase ID: " + id);
@@ -188,14 +112,14 @@ public class LanguageManager
 			}
 		}
 	}
-	
+
 	/**
 	 * @return	The number of phrases containing the provided string in their ID.
 	 */
 	public int getNumberOfPhrasesMatchingID(String id)
 	{
 		List<String> containingKeys = new ArrayList<String>();
-		
+
 		for (String key : translationsMap.keySet())
 		{
 			if (key.contains(id))
@@ -203,7 +127,58 @@ public class LanguageManager
 				containingKeys.add(key);
 			}
 		}
-		
+
 		return containingKeys.size();
+	}
+
+	public void loadLanguage(String languageId)
+	{
+		//Remove old translations.
+		translationsMap.clear();
+		
+		//Set up properties instance.
+		Properties properties = new Properties();
+		
+		//Can only load languages client-side.
+		if (FMLCommonHandler.instance().getEffectiveSide().isClient())
+		{
+			try
+			{
+				properties.load(StringTranslate.class.getResourceAsStream("/assets/" + modId + "/lang/" + languageId + ".lang"));
+			}
+
+			catch (Exception e)
+			{
+				RadixCore.getLogger().error("Error loading language " + languageId + " for " + modId + ". Attempting to default to English.");
+
+				try
+				{
+					properties.load(StringTranslate.class.getResourceAsStream("/assets/" + modId + "/lang/" + "en_US.lang"));
+				}
+
+				catch (Exception e2)
+				{
+					RadixExcept.logFatalCatch(e2, "Error loading language " + languageId + " for mod " + modId + ".");
+				}
+			}
+		}
+
+		else
+		{
+			try
+			{
+				properties.load(StringTranslate.class.getResourceAsStream("/assets/" + modId + "/lang/" + "en_US.lang"));
+			}
+
+			catch (Exception e)
+			{
+				RadixExcept.logFatalCatch(e, "Error loading language server-side. Loading cannot continue.");
+			}
+		}
+
+		for (final Map.Entry<Object, Object> entrySet : properties.entrySet())
+		{
+			translationsMap.put(entrySet.getKey().toString(), entrySet.getValue().toString());
+		}
 	}
 }
